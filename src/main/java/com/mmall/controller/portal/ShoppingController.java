1f03b715ce2f5d2696f2ab90919345c4ceaf4420
cuzz1 <cuzz1234@163.com>
2018-07-29 19:15:55 +0800

    modify ftp

---
+++ src/main/java/com/mmall/controller/portal/ShoppingController.java
@@ -0,0 +1,10 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shopping/")
+public class ShoppingController {
+
+}
